609c274d188bb15acb7ac8360470b4b5ca3a8e07
Nyoman Pastika <113783677+passchatter@users.noreply.github.com>
2024-12-24 12:36:21 +0800

    merubah index.php

--- api/produk.php
+++ api/produk.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-include '../database.php';
+include '../index.php';
 
 function execute_query($sql, $types = '', $params = []) {
     global $conn;
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo 'nadishop';
+$host = 'localhost';
+$user = 'root';
+$pass = 'root';
+$dbname = 'nadiashop';
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+if($conn->connect_error){
+    die("koneksi gagal: ". $conn->connect_error);
+}
 
 ?>
