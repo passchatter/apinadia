ab8d9fa3024b7109b9f69662e04ce707924828b5
Nyoman Pastika <113783677+passchatter@users.noreply.github.com>
2024-12-25 18:05:48 +0800

    update nadia

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $user = 'root';
 $pass = 'root';
 $dbname = 'nadiashop';
 
+
 $conn = new mysqli($host, $user, $pass, $dbname);
 if($conn->connect_error){
     die("koneksi gagal: ". $conn->connect_error);
